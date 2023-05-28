85685e7fc0d83f3ebfc5bcedf28d3ff2d54a5843
Uday Rakhelkar <uday.rakhelkar@soroco.com>
2023-05-28 10:44:15 +0530

    added changes for key

--- Practice Exercise/Excercise-1.py
+++ Practice Exercise/Excercise-1.py
@@ -1,37 +1,39 @@
 """
 Make a class that represents a bank account. Create four methods named set_details, display,
-withdraw and deposit.
+withdraw and amount.
 
 In the set_details method, create two instance variables : name and balance.
 The default value for balance should be zero. In the display method,
 display the values of these two instance variables.
 
-Both the methods withdraw and deposit have amount as parameter.
-Inside withdraw, subtract the amount from balance and inside deposit,
+Both the methods withdraw and amount have amount as parameter.
+Inside withdraw, subtract the amount from balance and inside amount,
 add the amount to the balance.
 
 Create two instances of this class and call the methods on those instances.
 
 
 """
+
+
 class BankAccount:
 
     def __init__(self):
-        self.value = None
+        self.balance = None
         self.name = None
 
-    def set_details(self, name, value=0):
+    def set_details(self, name, balance=0):
         self.name = name
-        self.value = value
+        self.balance = balance
 
     def display(self):
-        print(fr'name of the account holder : {self.name}, ammount : {self.value}')
+        print(fr'name of the account holder : {self.name}, ammount : {self.balance}')
 
-    def withdraw(self, deduct):
-        self.value-=deduct
+    def withdraw(self, amount):
+        self.balance -= amount
 
-    def deposit(self, deposit):
-        self.value+=deposit
+    def amount(self, amount):
+        self.balance += amount
 
 
 a1 = BankAccount()
@@ -44,7 +46,7 @@ a1.display()
 a2.display()
 
 a1.withdraw(100)
-a2.deposit(500)
+a2.amount(500)
 
 a1.display()
 a2.display()
